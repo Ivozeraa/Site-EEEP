c683d068fef0e50528beac2de0eb4a7f6c00ccc1
Erikadas <erikramon3331@gmail.com>
2024-10-20 13:51:13 -0300

    cursos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'; // Correção da biblioteca
-import './components-css/Footer.css'; 
+import styles from './components-css/Footer.css'; // Correto uso de CSS modules
 
 function Footer() {
     return (
-        <div className="Footer">
+        <div className={styles.Footer}>
             <footer>
                 <p>© 2024 Irmã Ana Zélia da Fonseca - Todos os direitos reservados.</p>
                 <a href="https://www.instagram.com/epirmaanazelia?igsh=eHFxNHl0NnNlNjVh" aria-label="Instagram">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,47 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faGraduationCap, faUser } from '@fortawesome/free-solid-svg-icons';
-import styles from "./components-css/Header.css"; // CSS module
+import { faHome, faGraduationCap, faUser, faBolt, faCode, faSyringe, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import styles from "./components-css/Header.css"; 
 
 function Header() {
+    const [showList, setShowList] = useState(false); 
+    
+const toggleList = () => {
+        setShowList(!showList); 
+    };
+
     return (
         <div className={styles.Header}>
             <div className='imgPrincipal'> </div>
             <header>
+
                 <img src="/logoEEEP.png" alt="EEEP Logo" />
                 <h1>EEEP Irmã Ana Zélia da Fonseca</h1>
+                
                 <nav>
-                    <a href="#"> 
+
+                    <a href="#" className="option"> 
                         <FontAwesomeIcon icon={faHome} />
                     </a>
-                    <a href="#"> 
-                        <FontAwesomeIcon icon={faGraduationCap} />
-                    </a>
-                    <a href="#">
+
+                    <div style={{ position: 'relative' }}  >
+                        <a onClick={toggleList} className="option"> 
+                            <FontAwesomeIcon icon={faGraduationCap}  />
+                        </a>
+
+                        <ul className={showList ? 'showList' : ''}>
+                            <li><a href="#"><FontAwesomeIcon icon={faEnvelope} />Administração</a></li>
+                            <li><a href="#"><FontAwesomeIcon icon={faBolt} />Eletrotecnica</a></li>
+                            <li><a href="#"><FontAwesomeIcon icon={faSyringe} />Enfermagem</a></li>
+                            <li><a href="#"><FontAwesomeIcon icon={faCode} />Informática</a></li>
+                        </ul>
+
+                    </div>
+                    <a href="#" className='login'>
                         <FontAwesomeIcon icon={faUser} />
                         <p>Login</p>
                     </a>
+
                 </nav>
             </header>
         </div>
